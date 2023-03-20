e81fb9477e5b1058e1718e7cdce4eafdeb8b91ea
ThandekileKheswa <thandekilekheswa@outlook.com>
2023-03-20 13:18:31 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - it is the main function
+ * Return: it is always 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %ln byte(s)\n", sizeof(char));
+	printf("Size of an int: %ln byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ln byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ln byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ln byte(s)", sizeof(float));
+	return (0);
+}
