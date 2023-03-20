9bd57447ed9423d6a230e9a13343e6460f54f656
ThandekileKheswa <thandekilekheswa@outlook.com>
2023-03-20 12:35:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - it is the main function
+ *
+ * Return: it is always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	results (0);
+}
