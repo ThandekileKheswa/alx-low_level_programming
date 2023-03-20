0482fe06790922e3a9882965eea35c452fb7b3e3
ThandekileKheswa <thandekilekheswa@outlook.com>
2023-03-20 15:14:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdio.h>
+
 /**
- * main - a c program that prints a line to the standard error
- * Return: 1 success
+ * main - it is the main function
+ *
+ * Return: there is always 1 success
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
